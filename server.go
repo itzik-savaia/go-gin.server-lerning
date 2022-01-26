7294c9702cbee6a454cdd117ace7461203339bf9
itzikking <itzikking1@gmail.com>
2022-01-26 21:08:16 +0200

    secound commit

--- server.go
+++ server.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"io"
+	"os"
+
 	"github.com/gin-gonic/gin"
+	gindump "github.com/tpkeeper/gin-dump"
 	"main.go/controller"
+	"main.go/middlewares"
 	"main.go/service"
 )
 
@@ -11,8 +16,19 @@ var (
 	VideoController controller.VideoController = controller.New(videoService)
 )
 
+func setupLogOutput() {
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
+
 func main() {
-	server := gin.Default()
+
+	setupLogOutput()
+
+	server := gin.New()
+
+	server.Use(gin.Recovery(), middlewares.Logger(),
+		middlewares.BasicAuth(), gindump.Dump())
 
 	server.GET("/videos", func(c *gin.Context) {
 		c.JSON(200, VideoController.FindAll())
